39c6eea1922411a7e51f6406da43d69d46f2b864
Daman <aroradaman@gmail.com>
2022-01-03 18:54:03 +0530

    suit and rank system

---
+++ rank.go
@@ -0,0 +1,28 @@
+package deck
+
+type Rank struct {
+	index uint8
+	name  string
+}
+
+var (
+	Two   = Rank{index: 2, name: "Two"}
+	Three = Rank{index: 3, name: "Three"}
+	Four  = Rank{index: 4, name: "Four"}
+	Five  = Rank{index: 5, name: "Five"}
+	Six   = Rank{index: 6, name: "Six"}
+	Seven = Rank{index: 7, name: "Seven"}
+	Eight = Rank{index: 8, name: "Eight"}
+	Nine  = Rank{index: 9, name: "Nine"}
+	Ten   = Rank{index: 10, name: "Ten"}
+	Jack  = Rank{index: 11, name: "Jack"}
+	Queen = Rank{index: 12, name: "Queen"}
+	King  = Rank{index: 13, name: "King"}
+	Ace   = Rank{index: 14, name: "Ace"}
+)
+
+var RANKS = [...]Rank{Ace, King, Queen, Jack, Ten, Nine, Eight, Seven, Six, Five, Four, Three, Two}
+
+func (r Rank) String() string {
+	return r.name
+}
---
+++ suit.go
@@ -0,0 +1,19 @@
+package deck
+
+type Suit struct {
+	index uint8
+	name  string
+}
+
+var (
+	Spade   = Suit{index: 3, name: "Spade"}
+	Club    = Suit{index: 2, name: "Club"}
+	Diamond = Suit{index: 1, name: "Diamond"}
+	Heart   = Suit{index: 0, name: "Heart"}
+)
+
+var SUITS = [...]Suit{Spade, Club, Diamond, Heart}
+
+func (s Suit) String() string {
+	return s.name
+}
